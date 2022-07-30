dcc0e337feb4078ddb311f3500da57320841da59
Aaka18sh <82805453+Aaka18sh@users.noreply.github.com>
2022-07-30 09:05:43 +0530

    new

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,10 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Aakash Singh Rajput</span> i'm 19 years old. currently, i'm living in Ahmedabad Gujrat and also study in Ahmedabad at silveroak University. currently my 5th sem is running in btech(IT). ",
+        "In past few year i'm working in frontend web devolpement and now i want to work as well in backend.  "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
